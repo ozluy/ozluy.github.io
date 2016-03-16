e7758de4d0bbec97fc036485a51bdbbb816fd6e5
Yusuf Özlü <ozluyusuf@gmail.com>
2016-03-16 23:26:57 +0200

    some changes
    some changes

--- projects/netend-game/vendor/js/game.js
+++ projects/netend-game/vendor/js/game.js
@@ -38,25 +38,34 @@ var spin = function () {
     var random = Math.floor((Math.random() * 6));
     selectedSymbolValue = selectedSymbol.options[selectedSymbol.selectedIndex].value
     console.log(selectedSymbolValue);
+
+    function hideMessage() {
+        winContainer.className = "";
+        lostContainer.className = "";
+        audioWon.pause()
+        audioWon.currentTime = 0;
+        audioLost.pause()
+        audioLost.currentTime = 0;
+    };
     var selectedByMachine = listOfSymbols[random];
     if (selectedByMachine.name == selectedSymbolValue) {
         console.log("machine: " + selectedByMachine.name + ", your select:" + selectedSymbolValue);
-        symbolContainer.innerHTML = '<img class="symbol" alt="' + selectedByMachine.name + '" src="vendor/img/' + selectedByMachine.imgUrl + '" />';
-        winContainer.className = "active";
-        audioWon.play()
+        symbolContainer.innerHTML = '<img class="symbol animated" alt="' + selectedByMachine.name + '" src="vendor/img/' + selectedByMachine.imgUrl + '" />';
+        function showMessageWon() {
+            winContainer.className = "active";
+            audioWon.play();
+        };
+        setTimeout(showMessageWon, 500);
+        setTimeout(hideMessage, 3500);
     }
     else {
         console.log("machine: " + selectedByMachine.name + ", your select:" + selectedSymbolValue);
-        symbolContainer.innerHTML = '<img class="symbol" alt="' + selectedByMachine.name + '" src="vendor/img/' + selectedByMachine.imgUrl + '" />';
-        lostContainer.className = "active";
-        audioLost.play()
+        symbolContainer.innerHTML = '<img class="symbol animated" alt="' + selectedByMachine.name + '" src="vendor/img/' + selectedByMachine.imgUrl + '" />';
+        function showMessageLost() {
+            lostContainer.className = "active";
+            audioLost.play();
+        };
+        setTimeout(showMessageLost, 500);
+        setTimeout(hideMessage, 3500);
     }
 };
-var restart = function () {
-    audioWon.pause()
-    audioWon.currentTime = 0;
-    audioLost.pause()
-    audioLost.currentTime = 0;
-    winContainer.className = "";
-    lostContainer.className = "";
-}
