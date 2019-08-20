c5c9934d5ff0256a2e0dd59011e2080e46bd08d1
Yusuf Özlü <y.ozlu@cleo.one>
2019-08-21 00:14:43 +0200

    deploy rematch demo

---
+++ projects/rematch-demo/precache-manifest.9434836afdac60761febc16d2a88019e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13d49f5dc89c0187cd8028af707432a",
+    "url": "/projects/rematch-demo/index.html"
+  },
+  {
+    "revision": "5d7f3fe91ac7475c000b",
+    "url": "/projects/rematch-demo/static/css/main.3bb60307.chunk.css"
+  },
+  {
+    "revision": "a6dd3e5af3fadaaa8b22",
+    "url": "/projects/rematch-demo/static/js/2.febdd4ff.chunk.js"
+  },
+  {
+    "revision": "5d7f3fe91ac7475c000b",
+    "url": "/projects/rematch-demo/static/js/main.a3f2b5cc.chunk.js"
+  },
+  {
+    "revision": "ea782e61fb9c8ef33f6d",
+    "url": "/projects/rematch-demo/static/js/runtime~main.e98c1cd1.js"
+  }
+]);
